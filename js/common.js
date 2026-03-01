2e1de6f14dc26fbc729e954e76b1287abdb76c1e
H00zy <48548882+H00zy@users.noreply.github.com>
2026-03-01 07:23:26 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -264,3 +264,8 @@ function formatMMSS(totalSeconds){
   const ss = String(s).padStart(2,"0");
   return toArabicDigits(`${mm}:${ss}`);
 }
+
+function toArabicDigits(input){
+  const map = { "0":"٠","1":"١","2":"٢","3":"٣","4":"٤","5":"٥","6":"٦","7":"٧","8":"٨","9":"٩" };
+  return String(input).replace(/[0-9]/g, d => map[d]);
+}
